c8b3c565517ffc41d1b14c45760f22fd46b13f2f
Danielzhel <fartunazh@gmail.com>
2022-10-25 20:18:55 +0300

    Добавьте файлы проекта.

---
+++ Human/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Human
+{
+    public class Person
+    { 
+        public int age { get; set; }
+
+        public void Great()
+        {
+            Console.WriteLine("Hello");
+
+        }
+        public void SetAge(int age)
+        {
+
+        }
+    }
+}
---
+++ Human/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Human
+{
+    public class Student : Person
+    {
+        public void Study()
+        {
+            Console.WriteLine("I'm student. I'm study");
+
+        }
+        public void ShowAge(age)
+        {
+            Console.WriteLine($"I'm {age} years old");
+        }
+    }
+}
